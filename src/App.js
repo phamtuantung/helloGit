60acddce2048b82557fe03cd1e3b10dfcb0fafd9
Ptuantung <ptuantung@gmail.com>
2020-01-06 23:37:09 +0700

    app js mới nạ

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// class App extends React.Component {
+//    render() {
+//       return (
+//          <div>
+//             Hello World!!!
+//             const name = 'Josh Perez';
+//             const element = <h1>Hello, {name}</h1>;
+//          </div>
+//       );
+//    }
+// }
+// export default App;
